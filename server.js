68e44b0a941c07c7e4adbb96277062dc1d0b5ca2
Jung In Kim <vindictuslbp@gmail.com>
2023-05-18 13:57:27 -0400

    Added server.js file to start the server under port 3000+. Added in variables for requirement usage.

---
+++ server.js
@@ -0,0 +1,9 @@
+// Global variable to store module requirements.
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+// Global variable to initialize and set up express server.
+const app = express();
+const PORT = process.env.PORT || 3000;
+
