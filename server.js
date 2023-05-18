e0a93995e3aa19f931571aa622f7de23bc9a7798
Jung In Kim <vindictuslbp@gmail.com>
2023-05-18 19:38:52 -0400

    Added code into server.js to apply routes for the application to retrieve & send notes.

--- server.js
+++ server.js
@@ -7,3 +7,23 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Middleware functions to set up express applications.
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// Routes for the API usage.
+app.get('/api/notes', (req, res) => {
+  // Reads content from the db.json file.
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    // If statement for potential errors.
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: 'Failed to read the database!' });
+    }
+    
+    // Parse the array data then send it as a response to the client.
+    const notes = JSON.parse(data);
+    res.json(notes);
+  }); 
+});
