68d98003f27ffb1f5fe67246e0993c80d84411dd
Jung In Kim <vindictuslbp@gmail.com>
2023-05-19 22:22:10 -0400

    Added more code to create an API route for creating and saving new notes into the navigation and database.

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/api/notes', (req, res) => {
     // If statement for potential errors.
     if (err) {
       console.error(err);
-      return res.status(500).json({ error: 'Failed to read the database!' });
+      return res.status(500).json({ error: 'Failed to read the note database!' });
     }
     
     // Parse the array data then send it as a response to the client.
@@ -28,3 +28,36 @@ app.get('/api/notes', (req, res) => {
   }); 
 });
 
+// API route for creating and saving a new note onto the right navigation side.
+app.post('/api/notes', (req, res) => {
+  // Reads the notes from the db.json file.
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    // If statement to check for potential errors.
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: 'Failed to read the note database!' });
+    }
+
+    // Parse the data as JSON into a variable.
+    const notes = JSON.parse(data);
+
+    // Generates a unique ID for the newly saved note.
+    const newNote = req.body;
+    newNote.id = Date.now();
+
+    // Add the new note into the array.
+    notes.push(newNote);
+
+    // Write the updated notes to the db.json file for storage.
+    fs.writeFile('./db/db.json', JSON.stringify(notes, null, 2), (err) => {
+      // If statement to check for any potential errors.
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Failed to save the note to the database!' });
+      }
+
+      // Returns the new note as a response.
+      res.json(newNote);
+    });
+  });
+});
